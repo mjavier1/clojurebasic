73594d4dd138a173a548244f70e0f1f2676afa97
mjavier1 <mjaguirre5@gmail.com>
2021-07-05 20:24:11 -0400

    data varaible
    difference

---
+++ dataVariable.clj
@@ -0,0 +1,9 @@
+(def aLong 15 )
+(def aDouble 3.14 )
+(def aString "Heloo" )
+(def aBoolean true )
+
+(type aLong) (type aDouble) (type aString) (type aBoolean)
+(nil? aString)
+(pos? aLong)
+(neg? aLong)
