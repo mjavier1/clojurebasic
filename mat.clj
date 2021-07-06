cc2868a81094ad6cc627173fef32c43733c3b8aa
mjavier1 <mjaguirre5@gmail.com>
2021-07-06 14:54:37 -0400

    mat

---
+++ mat.clj
@@ -0,0 +1,8 @@
+ (println "add : " (+ 1 2 3))
+  (println "Subtract : " (- 8 2 3))
+   (println "mul : " (* 1 2 3))
+    (println "divide : " (/ 10 5))
+     (println "modules : " (mod 12 5))
+      (println "inc: " (inc 10))
+       (println "rand-int : " (rand-int 20))
+       
