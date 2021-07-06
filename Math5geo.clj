46b1cecb915398f1c64144b9805aa005c7bb1f7f
mjavier1 <mjaguirre5@gmail.com>
2021-07-06 15:48:11 -0400

    Create Math5geo.clj

---
+++ Math5geo.clj
@@ -0,0 +1,14 @@
+(println (format "Hypot :%.3f" (Math/hypot 2 2)) )
+(println "cos : " (Math/cos 0))
+(println "sin : " (Math/sin 0))
+(println "tan : " (Math/tan 0))
+
+
+(println " acos: "    (Math/acos 0))
+(println "asin : " (Math/asin 0))
+(println "atan : " (Math/atan 0))
+
+
+(println "cosh : " (Math/cosh 0))
+(println "sinh : " (Math/sinh 0))
+(println "tanh : " (Math/tanh 0))
